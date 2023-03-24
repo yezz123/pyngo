be7899691cd3b824766319f70397b56ce76ee21c
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2023-03-24 16:50:11 +0700

    :bug: Skip empty `string` if the field does not expect `string` type (#45)
    * Skip empty string if the field does not expect string type
    
    * Increase test coverage
    
    * Format code

--- tests/querydict_test.py
+++ tests/querydict_test.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Optional, Union
 
 import pytest
 from django.http import QueryDict
@@ -13,13 +13,23 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
 class Model(QueryDictModel, BaseModel):
     foo: int
     bar: List[int]
+    sub_id: Optional[int]
+    key: str = "key"
 
 
 @pytest.mark.parametrize(
     ("data", "expected"),
     (
-        (QueryDict("foo=12&bar=12"), Model(foo=12, bar=[12])),
-        ({"foo": 44, "bar": [0, 4]}, Model(foo=44, bar=[0, 4])),
+        (QueryDict("foo=12&bar=12"), Model(foo=12, bar=[12], key="key")),
+        ({"foo": 44, "bar": [0, 4]}, Model(foo=44, bar=[0, 4], key="key")),
+        (
+            QueryDict("foo=10&bar=12&sub_id=&key="),
+            Model(foo=10, bar=[12], sub_id=None, key=""),
+        ),
+        (
+            QueryDict("foo=10&bar=12&key=abc&extra=something"),
+            Model(foo=10, bar=[12], key="abc"),
+        ),
     ),
 )
 def test_parsing_objects(
@@ -29,3 +39,5 @@ def test_parsing_objects(
     assert got == expected
     assert got.foo == expected.foo
     assert got.bar == expected.bar
+    assert got.sub_id == expected.sub_id
+    assert got.key == expected.key
