9d04a467d650f5fdd4133be2f14edefc8c05f535
Yasser Tahiri <yasserth19@gmail.com>
2023-04-02 19:14:19 +0000

    🎉 Release 1.6.0

--- pyngo/__init__.py
+++ pyngo/__init__.py
@@ -1,6 +1,6 @@
 """Pydantic Package for Adding Models into a Django or Django Rest Framework Project"""
 
-__version__ = "1.5.0"
+__version__ = "1.6.0"
 
 from .errors import drf_error_details
 from .openapi import ParameterDict, openapi_params
